753311992531bde79305171f01cd5083534093ed
joy Kurui <kurui.chepngetich@gmail.com>
2023-10-07 00:57:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function  appendCat(name){
+    const newArray = [...cats, name];
+    return newArray;
+}
+appendCat("Broom");
+
+function prependCat(name){
+    const newArray2 = [name, ...cats];
+    return newArray2;
+}
+function removeLastCat(){
+    const newArray3 = cats.slice(0, -1);
+    return newArray3;
+}
+removeLastCat();
+
+function removeFirstCat(){
+    const newArray4 = cats.slice(1);
+    return newArray4;
+}
+removeFirstCat();
